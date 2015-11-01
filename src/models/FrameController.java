15af21cc7251a10acf8279d48b16d8852e236ff7
ajnaber <Alexander.naber1@gmail.com>
2015-11-01 15:39:25 -0800

    Initial commit

---
+++ src/models/FrameController.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 11/1/2015.
+ */
+public class FrameController {
+
+    private ArrayList cardList;
+    private User user;
+
+}
---
+++ src/models/RegisterModel.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by Alex on 11/1/2015.
+ */
+public class RegisterModel {
+}
